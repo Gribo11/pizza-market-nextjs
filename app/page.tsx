122bee24d7fa8af4699aef9e26158afe8a25fd6e
Sergey Kuzmenko <s.kuzmenko@sense.pro>
2024-08-27 13:34:43 +0300

    improve filters and add prod crd

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,4 @@
-import { Container, Filters, Title, TopBar } from "@/components/shared/index";
+import { Container, Filters, ProductCard, Title, TopBar } from "@/components/shared/index";
 
 export default function Home() {
   return (
@@ -6,18 +6,24 @@ export default function Home() {
       <Container className="mt-10">
         <Title text="All pizzas" />
       </Container>
+
       <TopBar />
-      <Container className="pb-14">
+
+      <Container className="mt-14 pb-14">
         <div className="flex gap-[60px]">
           <div className="w-[250px]">
             <Filters />
           </div>
-        </div>
+
 
         <div className="flex-1">
-          <div className="flex flex-col gap-16"></div>
+          <div className="flex flex-col gap-16">
+            <ProductCard id={1} name='text' price={500} imageUrl="https://media.dodostatic.net/image/r:292x292/11EEFB595A197C24BA932A0AD1144AFB.jpg"/>
+          </div>
         </div>
-      </Container>
+
+        </div>
+      </Container>  
     </>
   );
 }
--- components/shared/filters.tsx
+++ components/shared/filters.tsx
@@ -1,5 +1,10 @@
+'use client';
+
 import React from "react";
+import { Input } from "../ui/input";
+import { CheckboxFiltersGroup } from "./checkbox-filters-group";
 import { FilterCheckbox } from "./filter-checkbox";
+import { RangeSlider } from "./range-slider";
 import { Title } from "./title";
 
 interface Props {
@@ -15,6 +20,108 @@ export const Filters: React.FC<Props> = ({ className }) => {
         <FilterCheckbox text="Can make" value="2" />
         <FilterCheckbox text="New" value="1" />
       </div>
+      <div className="mt-5 border-y border-y-neutral-100 py-6 pb-7">
+        <p className="font-bold mb-3">Цена от и до:</p>
+        <div className="flex gap-3 mb-5">
+          <Input type="number" placeholder="0" min={0} max={1000} />
+          <Input type="number" min={100} max={1000} placeholder="1000" />
+        </div>
+        <RangeSlider min={0} max={1000} step={10} value={[0, 5000]} />
+      </div>
+
+
+      <CheckboxFiltersGroup
+        title="Ингредиенты"
+        name="ingredients"
+        className="mt-5"
+        limit={6}
+        defaultItems={[
+          {
+            "text": "Thin Crust",
+            "value": "1"
+          },
+          {
+            "text": "Traditional Crust",
+            "value": "2"
+          },
+          {
+            "text": "Cheese Lovers",
+            "value": "3"
+          },
+          {
+            "text": "Pepperoni Feast",
+            "value": "4"
+          },
+          {
+            "text": "Vegetarian",
+            "value": "5"
+          },
+    
+        ]}
+        items={[
+          {
+            "text": "Thin Crust",
+            "value": "1"
+          },
+          {
+            "text": "Traditional Crust",
+            "value": "2"
+          },
+          {
+            "text": "Cheese Lovers",
+            "value": "3"
+          },
+          {
+            "text": "Pepperoni Feast",
+            "value": "4"
+          },
+          {
+            "text": "Vegetarian",
+            "value": "5"
+          },
+          {
+            "text": "Meat Lovers",
+            "value": "6"
+          },
+          {
+            "text": "BBQ Chicken",
+            "value": "7"
+          },
+          {
+            "text": "Hawaiian",
+            "value": "8"
+          },
+          {
+            "text": "Buffalo Chicken",
+            "value": "9"
+          },
+          {
+            "text": "Supreme",
+            "value": "10"
+          },
+          {
+            "text": "Margherita",
+            "value": "11"
+          },
+          {
+            "text": "Four Cheese",
+            "value": "12"
+          },
+          {
+            "text": "Seafood Delight",
+            "value": "13"
+          },
+          {
+            "text": "Spicy Italian",
+            "value": "14"
+          },
+          {
+            "text": "Mushroom & Truffle",
+            "value": "15"
+          }
+        ]}
+       
+      />
     </div>
   );
 };
